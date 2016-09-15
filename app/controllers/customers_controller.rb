502b3c55658540b0e78c63d05a86665757cf953a
Jerome Hardaway <jeromedhardaway@gmail.com>
2016-09-15 09:40:33 -0500

    added _pager.html.erb

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,8 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
@@ -7,6 +10,7 @@ class CustomersController < ApplicationController
           customer_search_term.where_clause,
           customer_search_term.where_args).
         order(customer_search_term.order)
+        offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
     end
